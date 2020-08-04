e2503f94a82827fcddcb2c2042d82a73157ca9ea
furkan-coder <firstinfirstout2020@gmail.com>
2020-08-05 02:28:23 +0300

    admin pages modified and models added

--- quotes/admin.py
+++ quotes/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Quote
 
 # Register your models here.
+
+class QuoteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'created_date', 'isPublished')
+    list_display_links = ('id', 'name')
+    list_filter = ('created_date',)
+    list_editable = ('isPublished',)
+    search_fields = ('name', 'description')
+    list_per_page = 20
+
+admin.site.register(Quote, QuoteAdmin)
--- quotes/models.py
+++ quotes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Quote(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Söz Konusu')
+    description = models.TextField(verbose_name='Söz')
+    image = models.CharField(max_length=50, verbose_name='Resim')
+    created_date = models.DateTimeField(auto_now_add=True)
+    isPublished = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
