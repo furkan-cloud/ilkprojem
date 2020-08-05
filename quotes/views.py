3f4a34702ebb617d6a6bb07917bd48c2760273c1
furkan-coder <firstinfirstout2020@gmail.com>
2020-08-06 02:04:10 +0300

    admin panel activated

--- quotes/views.py
+++ quotes/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
+from django.http import Http404
+from .models import Quote
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'quotes/list.html')
+    quotes = Quote.objects.all()
+    context = {
+        'quotes' : quotes
+    }
+    return render(request, 'quotes/list.html', context)
 
-def detail(request):
-    return render(request, 'quotes/detail.html')
+def detail(request, quote_id):
+    try:
+        quote = Quote.objects.get(pk = quote_id)
+    except Quote.DoesNotExist:
+        raise Http404('Aradaığınız kayıt yok')
+    return render(request, 'quotes/detail.html', quote)
 
 def search(request):
     return render(request, 'quotes/search.html')
