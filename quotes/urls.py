9b47620858c83bf49c836c1791d9335f06c86902
furkan-coder <firstinfirstout2020@gmail.com>
2020-08-10 10:52:05 +0300

    files updated

--- quotes/urls.py
+++ quotes/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='quotes'),
     path('<int:quote_id>', views.detail, name= 'detail'),
-    path('search', views.search, name= 'search')
+    path('search', views.search, name= 'search'),
+    path('create', views.create, name= 'create'),
+    path('update', views.update, name= 'update'),
+    path('<int:quote_id>/delete', views.delete, name= 'delete'),
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404
 from django.http import Http404
 from .models import Quote
 
@@ -12,11 +13,22 @@ def index(request):
     return render(request, 'quotes/list.html', context)
 
 def detail(request, quote_id):
-    try:
-        quote = Quote.objects.get(pk = quote_id)
-    except Quote.DoesNotExist:
-        raise Http404('Aradaığınız kayıt yok')
-    return render(request, 'quotes/detail.html', quote)
+    quote = get_object_or_404(Quote, pk = quote_id)
+    context = {
+        'quote' : quote
+    }
+    return render(request, 'quotes/detail.html', context)
 
 def search(request):
     return render(request, 'quotes/search.html')
+
+def create(request):
+    return render(request, 'quotes/create.html')
+
+def update(request):
+    return render(request, 'quotes/update.html')
+
+def delete(request, quote_id):
+    quote = get_object_or_404(Quote, pk = quote_id)
+    quote.delete()
+    return redirect('quotes:search')
